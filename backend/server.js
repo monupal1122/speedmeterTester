94e8de859496db21fa86a7815dd39ce604cfee46
monupal1122 <monupalji1122@gmail.com>
2026-01-14 18:03:05 +0530

    update mongo

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 
 (async () => {
   // Database Connection
-  const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://Monu:Monu123@cluster0.kp6ma.mongodb.net/Internet";
+  const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/speedtest";
   try {
     await mongoose.connect(MONGODB_URI);
     log("Connected to MongoDB");
